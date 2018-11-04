36c3c3f0f217e50be259508ba3af7d9bf74dfac7
gardheamsantos <gardheam.santos@yahoo.com.br>
2018-11-03 23:07:59 -0300

    corrrreção footer generics

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,40 +8,31 @@ class FooterE extends Component{
             <FooterSection type="middle">
                 <FooterDropDownSection title="Features">
                     <FooterLinkList>
-                        <a href="#">About</a>
-                        <a href="#">Terms</a>
-                        <a href="#">Partners</a>
-                        <a href="#">Updates</a>
+                        <a href="/">About</a>
+                        <a href="/">Terms</a>
+                        <a href="/">Partners</a>
+                        <a href="/">Updates</a>
                     </FooterLinkList>
                 </FooterDropDownSection>
                 <FooterDropDownSection title="Details">
                     <FooterLinkList>
-                        <a href="#">Specs</a>
-                        <a href="#">Tools</a>
-                        <a href="#">Resources</a>
-                    </FooterLinkList>
-                </FooterDropDownSection>
-                <FooterDropDownSection title="Technology">
-                    <FooterLinkList>
-                        <a href="#">How it works</a>
-                        <a href="#">Patterns</a>
-                        <a href="#">Usage</a>
-                        <a href="#">Products</a>
-                        <a href="#">Contracts</a>
+                        <a href="/">Specs</a>
+                        <a href="/">Tools</a>
+                        <a href="/">Resources</a>
                     </FooterLinkList>
                 </FooterDropDownSection>
                 <FooterDropDownSection title="FAQ">
                     <FooterLinkList>
-                        <a href="#">Questions</a>
-                        <a href="#">Answers</a>
-                        <a href="#">Contact Us</a>
+                        <a href="/">Dúvidads</a>
+                        <a href="/">Sugestões</a>
+                        <a href="/">Contact-me</a>
                     </FooterLinkList>
                 </FooterDropDownSection>
             </FooterSection>
             <FooterSection type="bottom" logo="Title">
                 <FooterLinkList>
-                    <a href="#">Help</a>
-                    <a href="#">Privacy & Terms</a>
+                    <a href="/">Help</a>
+                    <a href="/">Privacy & Terms</a>
                 </FooterLinkList>
             </FooterSection>
         </Footer>
